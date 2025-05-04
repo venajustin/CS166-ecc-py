24031677db7274ae00b921f41c75e33b172e6b88
venajustin <venajustin@gmail.com>
2025-05-04 04:28:31 -0700

    modulus in finite field

--- test.py
+++ test.py
@@ -30,3 +30,98 @@ for i in range(6):
     a = a.add(domain.g, domain)
 
 ctx.flush()
+
+
+
+domain = Domain(
+    4,
+    EllipticCurve(0, 1),
+    CurvePoint(2, 3),
+    6,
+    1
+)
+
+
+ctx = GraphContext()
+domain.draw(ctx)
+
+print(domain.g)
+a = domain.g
+for i in range(6):
+    if a is None:
+        break
+    print(a)
+    a.draw(ctx)
+
+    print("verify: ", domain.verifyPoint(a))
+
+    a = a.add(domain.g, domain)
+
+ctx.flush()
+
+
+
+# test 3
+domain = Domain(
+    15733,
+    EllipticCurve(1, 3),
+    CurvePoint(0,0), # unknown
+    0, # not needed
+    0 # not needed
+)
+P = CurvePoint(6, 15)
+P.draw(ctx)
+Q = CurvePoint(8, 1267)
+Q.draw(ctx)
+R = CurvePoint(2, 3103)
+R.draw(ctx)
+TwoP = P.add(P, domain)
+TwoP.draw(ctx)
+ThreeP = TwoP.add(P, domain)
+ThreeP.draw(ctx)
+ctx.flush()
+FourP1 = P.add(ThreeP, domain)
+FourP2 = TwoP.add(TwoP, domain)
+assoc1 = P.add(Q.add(R, domain), domain)
+assoc2 = P.add(Q, domain).add(R, domain)
+print("FourP2: ", FourP2)
+print("FourP2: ", FourP2)
+
+print("Associative point 1: ", assoc1)
+print("Associative point 2: ", assoc2)
+
+print("validity checks: ")
+print(domain.verifyPoint(P))
+print(domain.verifyPoint(Q))
+print(domain.verifyPoint(R))
+print(domain.verifyPoint(TwoP))
+print(domain.verifyPoint(ThreeP))
+print(domain.verifyPoint(FourP1))
+print(domain.verifyPoint(assoc1))
+
+#test 4
+domain = Domain(
+    4,
+    EllipticCurve(0, 7),
+    CurvePoint(2, 3),
+    6,
+    1
+)
+
+
+ctx = GraphContext()
+domain.draw(ctx)
+
+print(domain.g)
+a = domain.g
+for i in range(6):
+    if a is None:
+        break
+    print(a)
+    a.draw(ctx)
+
+    print("verify: ", domain.verifyPoint(a))
+
+    a = a.add(domain.g, domain)
+
+ctx.flush()
--- utility/eccMath.py
+++ utility/eccMath.py
@@ -13,27 +13,40 @@ class Domain:
 
     def draw(self, ctx):
 
-        sections = []
         if self.p is None:
 
             y, x = np.ogrid[-5:5:100j, -5:5:100j]
-            sections.append([x, y])
-        else:
+            ctx.plt.contour(y.ravel(), x.ravel(), (pow(y, 2) - pow(x, 3) - x * self.curve.a - self.curve.b), [0])
 
-            for i in range(5):
-                y, x = np.ogrid[-self.p + 1 + (self.p * i):self.p * i - 2:200j, -self.p + 1 + (self.p * i):self.p * i - 2:200j]
-                sections.append([x,y])
+        # else:
+        #     #  we can't plot the graph in this state, but we can graph integer points in this way
+        #     a = self.g
+        #     while a is not None:
+        #         a.draw(ctx)
+        #         a = a.add(self.g, self)
 
-        for [x, y] in sections:
-            if self.p is not None:
-                xmod = x % self.p
-                ymod = y % self.p
-            else:
-                xmod = x
-                ymod = y
-            ctx.plt.contour(xmod.ravel(), ymod.ravel(), pow(y, 2) - pow(x, 3) - x * self.curve.a - self.curve.b, [0])
         ctx.plt.title('$y^{2} = x^{3}$ + ' + str(self.curve.a) + " * x + " + str(self.curve.b))
 
+    def verifyPoint(self, point):
+        if point is None:
+            return True
+        if self.p is not None:
+            difference = (point.y ** 2 - (point.x ** 3 + self.curve.a * point.x + self.curve.b)) % self.p
+            return (
+                    .0001 > difference > -0.0001
+                    and
+                    0 <= point.x < self.p and 0 <= point.y < self.p
+            )
+        else:
+            difference = point.y ** 2 - (point.x ** 3 + (self.curve.a * point.x) + self.curve.b)
+            return .0001 > difference > -0.0001
+
+    def inv_mod_p(self, x):
+        if self.p is None: # real
+            return 1 / x
+        if x % self.p == 0: # finite field
+            raise ZeroDivisionError("no inverse for " + x + " in field")
+        return pow(x, self.p-2, self.p)
 
 
 
@@ -43,14 +56,7 @@ class EllipticCurve:
         self.a = a
         self.b = b
 
-    def verifyPoint(self, point):
-        if point == None:
-            return False
-        difference = point.y ** 2 - (point.x ** 3 + (self.a * point.x) + self.b)
-        if difference < .0001 and difference > -0.0001:
-            return True
-        else:
-            return False
+
 
     def __str__(self):
         return "Curve: a " + str(self.a) + " b " + str(self.b)
@@ -91,7 +97,7 @@ class CurvePoint:
             return None
 
         # the slope of the curve at this point
-        slope = ((3 * (self.x ** 2)) + domain.curve.a) / (2 * self.y)
+        slope = ((3 * (self.x ** 2)) + domain.curve.a) * domain.inv_mod_p(2 * self.y)
 
         newx = slope ** 2 - (2 * self.x)
         newy = (slope * (self.x - newx)) - self.y
@@ -107,7 +113,7 @@ class CurvePoint:
         if other.x == self.x and other.y == self.y:
             return self.double(domain)
 
-        slope = self.slopeTo(other)
+        slope = self.slopeTo(other, domain)
         if slope is None:
             return None
         if slope == 0:
@@ -122,14 +128,18 @@ class CurvePoint:
 
         return CurvePoint(newx, newy)
 
-    def slopeTo(self, other):
+    def slopeTo(self, other, domain):
         num = (self.y - other.y)
         den = (self.x - other.x)
         if den == 0:
             return None
-        return num / den
+        # in order to do division within this field we use the inverse fn
+        # return num / den
+        return num * domain.inv_mod_p(den)
 
     def draw(self, ctx):
         ctx.plt.plot(self.x, self.y, 'bo')
         ctx.plt.annotate('(' + str(self.x) + ',' + str(self.y) + ')', (self.x, self.y), textcoords="offset points", xytext=(10, 0), ha='left')
 
+    def inverse(self, domain):
+        return CurvePoint(self.x, (-self.y)%domain.p)
